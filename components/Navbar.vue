e365948f8f2b9deb111b1728ccbdb4cbc555cbd6
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2024-09-14 15:58:16 +0200

    ADD commentaires dans Navbar.vue, PageHeadervue,Parteners.vue et ScrollButton.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,127 +1,132 @@
 <script setup lang="ts">
-const route = useRoute();
-const navbar = ref<Element | null>(null);
+// Import necessary functions and hooks from Vue and other libraries
+const route = useRoute(); // Get the current route
+const navbar = ref<Element | null>(null); // Reference to the navbar element
 
-const auth = useAuth();
-const toast = useToast();
+const auth = useAuth(); // Authentication hook
+const toast = useToast(); // Toast notification hook
 
-const { isMenuOpen, refLinks, resetMenu, toggleMenu, toggleMenuWithSubLinks } =
-  useNavbar();
+// Destructure necessary functions and state from the useNavbar hook
+const { isMenuOpen, refLinks, resetMenu, toggleMenu, toggleMenuWithSubLinks } = useNavbar();
 
+// Destructure necessary functions from the useBackground hook
 const { changeBackground, onScroll } = useBackground({ navbar });
 
+// Lifecycle hook to run when the component is mounted
 onMounted(() => {
-  navbar.value = document.querySelector(".navbar");
+  navbar.value = document.querySelector(".navbar"); // Get the navbar element
 
-  changeBackground();
-  window.addEventListener("scroll", onScroll);
+  changeBackground(); // Change the background based on the current state
+  window.addEventListener("scroll", onScroll); // Add scroll event listener
 });
 
+// Watch for changes in the route path and update the background and menu state
 watch(
-  () => route.path,
-  () => {
-    changeBackground();
-    resetMenu();
-  },
+    () => route.path,
+    () => {
+      changeBackground(); // Change the background based on the new route
+      resetMenu(); // Reset the menu state
+    },
 );
 
+// Function to handle user sign out
 const onSignOut = (): void => {
   auth
-    .signOut({ callbackUrl: "/" })
-    .then(() => {
-      toast.add({
-        title: "Déconnexion",
-        description: "Vous avez été déconnecté avec succès",
+      .signOut({ callbackUrl: "/" }) // Sign out the user and redirect to the home page
+      .then(() => {
+        toast.add({
+          title: "Déconnexion", // Title of the toast notification
+          description: "Vous avez été déconnecté avec succès", // Description of the toast notification
+        });
+      })
+      .catch((error) => {
+        toast.add({
+          title: "Déconnexion", // Title of the toast notification
+          description: `Une erreur est survenue lors de la déconnexion ${error}`, // Error message in the toast notification
+          color: "red", // Color of the toast notification
+        });
       });
-    })
-    .catch((error) => {
-      toast.add({
-        title: "Déconnexion",
-        description: `Une erreur est survenue lors de la déconnexion ${error}`,
-        color: "red",
-      });
-    });
 };
 </script>
 
 <template>
+  <!-- Navbar container -->
   <nav class="navbar">
-    <h1
-      class="text-xl md:text-3xl text-white font-bold animate__animated animate__fadeInLeft animate__fast"
-    >
+    <!-- Navbar title with a link to the home page -->
+    <h1 class="text-xl md:text-3xl text-white font-bold animate__animated animate__fadeInLeft animate__fast">
       <NuxtLink to="/">Magasin Connecté 4.0</NuxtLink>
     </h1>
 
+    <!-- Navbar menu container -->
     <div class="navbar-menu-container">
+      <!-- Menu items, conditionally rendered based on isMenuOpen -->
       <ul class="menu" v-if="isMenuOpen">
         <li
-          class="animate__animated animate__fadeInDown animate__faster"
-          v-for="refLink in refLinks"
-          :key="refLink.name"
+            class="animate__animated animate__fadeInDown animate__faster"
+            v-for="refLink in refLinks"
+            :key="refLink.name"
         >
+          <!-- Render a button for each menu item, with different behavior based on the item name -->
           <UButton
-            v-if="
-              refLink.name !== 'Photovoltaïque' && refLink.name !== 'Retour'
-            "
-            :to="refLink.path"
-            variant="link"
-            size="xl"
-            >{{ refLink.name }}</UButton
-          >
+              v-if="refLink.name !== 'Photovoltaïque' && refLink.name !== 'Retour'"
+              :to="refLink.path"
+              variant="link"
+              size="xl"
+          >{{ refLink.name }}</UButton>
           <UButton
-            v-else-if="refLink.name === 'Retour'"
-            :icon="refLink.icon"
-            variant="link"
-            size="xl"
-            @click="refLink.onClick"
+              v-else-if="refLink.name === 'Retour'"
+              :icon="refLink.icon"
+              variant="link"
+              size="xl"
+              @click="refLink.onClick"
           />
-
           <UButton
-            v-else
-            variant="link"
-            size="xl"
-            @click="toggleMenuWithSubLinks"
-            >{{ refLink.name }}</UButton
-          >
+              v-else
+              variant="link"
+              size="xl"
+              @click="toggleMenuWithSubLinks"
+          >{{ refLink.name }}</UButton>
         </li>
       </ul>
 
+      <!-- Menu toggle button, conditionally rendered based on isMenuOpen -->
       <UButton
-        icon="i-heroicons-bars-3-16-solid"
-        size="md"
-        v-if="!isMenuOpen"
-        class="menu-icon text-3xl cursor-pointer animate__animated animate__flipInX animate__fast"
-        @click="toggleMenu"
-        variant="link"
+          icon="i-heroicons-bars-3-16-solid"
+          size="md"
+          v-if="!isMenuOpen"
+          class="menu-icon text-3xl cursor-pointer animate__animated animate__flipInX animate__fast"
+          @click="toggleMenu"
+          variant="link"
+      />
+      <UButton
+          icon="i-heroicons-x-mark-16-solid"
+          size="md"
+          v-if="isMenuOpen"
+          class="menu-icon text-3xl cursor-pointer animate__animated animate__flipInX animate__fast"
+          @click="toggleMenu"
+          variant="link"
       />
 
-      <UButton
-        icon="i-heroicons-x-mark-16-solid"
-        size="md"
-        v-if="isMenuOpen"
-        class="menu-icon text-3xl cursor-pointer animate__animated animate__flipInX animate__fast"
-        @click="toggleMenu"
-        variant="link"
-      />
-
+      <!-- Administration link with a tooltip -->
       <UTooltip text="Espace administration">
         <UButton
-          icon="i-heroicons-user-16-solid"
-          size="md"
-          class="menu-icon"
-          to="/administration"
-          variant="link"
+            icon="i-heroicons-user-16-solid"
+            size="md"
+            class="menu-icon"
+            to="/administration"
+            variant="link"
         />
       </UTooltip>
 
+      <!-- Sign out button with a tooltip, conditionally rendered based on authentication status -->
       <UTooltip text="Déconnexion" v-if="auth.status.value === 'authenticated'">
         <UButton
-          v-if="auth.status.value === 'authenticated'"
-          icon="i-heroicons-arrow-right-start-on-rectangle-16-solid"
-          size="md"
-          class="menu-icon"
-          variant="link"
-          @click="onSignOut"
+            v-if="auth.status.value === 'authenticated'"
+            icon="i-heroicons-arrow-right-start-on-rectangle-16-solid"
+            size="md"
+            class="menu-icon"
+            variant="link"
+            @click="onSignOut"
         />
       </UTooltip>
     </div>
@@ -129,5 +134,5 @@ const onSignOut = (): void => {
 </template>
 
 <style scoped>
-@import url("~/assets/css/navbar.css");
+@import url("~/assets/css/navbar.css"); /* Import external CSS for the navbar styling */
 </style>
--- components/Parteners.vue
+++ components/Parteners.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// Define a constant array of strings representing the image links for the partners
 const PARTENERS_IMAGES_LINK: string[] = [
   "parteners/ikea.svg.webp",
   "parteners/lucibel.webp",
@@ -14,41 +15,51 @@ const PARTENERS_IMAGES_LINK: string[] = [
 </script>
 
 <template>
+  <!-- Container for the partners section -->
   <div class="parteners__container">
+    <!-- Description section for the partners -->
     <div class="parteners__description">
+      <!-- Title for the partners section -->
       <h2 class="text-xl font-bold text-primary-500 text-left md:text-right">
         Nos partenaires
       </h2>
+      <!-- Subtitle for the partners section -->
       <h3 class="italic text-left md:text-right">
         lorem ipsum lorem ipsum lorem
       </h3>
+      <!-- Divider for the description section, visible on medium and larger screens -->
       <UDivider size="sm" class="hidden md:flex" />
+      <!-- Paragraph describing the partners -->
       <p class="text-left md:text-right">
         lorem ipsum lorem ipum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
         lorem ipsumlorem ipsum
       </p>
     </div>
 
+    <!-- Vertical divider, visible on medium and larger screens -->
     <UDivider
-      class="hidden md:flex"
-      size="sm"
-      orientation="vertical"
-      icon="i-heroicons-chevron-right-16-solid"
+        class="hidden md:flex"
+        size="sm"
+        orientation="vertical"
+        icon="i-heroicons-chevron-right-16-solid"
     />
 
+    <!-- Horizontal divider, visible on small screens -->
     <UDivider
-      class="md:hidden"
-      size="sm"
-      orientation="horizontal"
-      icon="i-heroicons-chevron-down-16-solid"
+        class="md:hidden"
+        size="sm"
+        orientation="horizontal"
+        icon="i-heroicons-chevron-down-16-solid"
     />
 
+    <!-- Container for the partner images -->
     <div class="parteners__images">
+      <!-- Loop through the PARTENERS_IMAGES_LINK array and render each image -->
       <div class="image" v-for="(image, index) in PARTENERS_IMAGES_LINK">
         <NuxtImg
-          :key="index"
-          :src="image"
-          :alt="`logo ${image.split('.')[0]}`"
+            :key="index"
+        :src="image"
+        :alt="`logo ${image.split('.')[0]}`"
         />
       </div>
     </div>
@@ -56,5 +67,5 @@ const PARTENERS_IMAGES_LINK: string[] = [
 </template>
 
 <style scoped>
-@import url("~/assets/css/parteners.css");
+@import url("~/assets/css/parteners.css"); /* Import external CSS for the partners styling */
 </style>
