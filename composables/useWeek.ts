3e41ffdd6f54f6be161404983ef67530feaaf9e1
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2024-09-16 16:09:01 +0200

    ADD commentaires useWeek.ts

--- composables/useWeek.ts
+++ composables/useWeek.ts
@@ -6,31 +6,42 @@ type UseWeekReturn = {
 };
 
 /**
+ * Returns the current week and the next week with their start and end dates.
  *
- * @returns return current week and next week
+ * @returns {UseWeekReturn} An object containing the current week number and an array of weeks with their names and values.
  */
 export const useWeek = (): UseWeekReturn => {
+  // Get the current week number of the year
   const currentWeekNumber: number = moment(new Date()).isoWeek();
+  // Get the current year
   const currentYear: number = moment(new Date()).year();
 
+  /**
+   * Gets the start and end dates of a given week number in a given year.
+   *
+   * @param {number} weekNumber - The week number.
+   * @param {number} year - The year.
+   * @returns {Object} An object containing the start and end dates of the week.
+   */
   const getStartAndEndOfWeek = (weekNumber: number, year: number) => {
     const startOfWeek = moment()
-      .year(year)
-      .week(weekNumber)
-      .startOf("week")
-      .add(1, "day");
+        .year(year)
+        .week(weekNumber)
+        .startOf("week")
+        .add(1, "day"); // Add 1 day to start from Monday
     const endOfWeek = moment()
-      .year(year)
-      .week(weekNumber)
-      .endOf("week")
-      .add(1, "day");
+        .year(year)
+        .week(weekNumber)
+        .endOf("week")
+        .add(1, "day"); // Add 1 day to end on Sunday
 
     return {
-      start: startOfWeek.format("DD/MM/YYYY"),
-      end: endOfWeek.format("DD/MM/YYYY"),
+      start: startOfWeek.format("DD/MM/YYYY"), // Format the start date
+      end: endOfWeek.format("DD/MM/YYYY"), // Format the end date
     };
   };
 
+  // Create an array of the current week and the next week with their start and end dates
   const weeks = [
     {
       name: `${getStartAndEndOfWeek(currentWeekNumber, currentYear).start} au ${getStartAndEndOfWeek(currentWeekNumber, currentYear).end}`,
@@ -42,6 +53,7 @@ export const useWeek = (): UseWeekReturn => {
     },
   ];
 
+  // Return the current week number and the array of weeks
   return {
     weeks,
     currentWeekNumber,
