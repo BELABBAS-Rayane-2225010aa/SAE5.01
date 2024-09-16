01396a9b9013531cf5af433f72f1c6ce21c63f2e
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2024-09-16 15:26:17 +0200

    ADD commentaires index.ts et useDate.ts

--- composables/index.ts
+++ composables/index.ts
@@ -1,3 +1,7 @@
+/**
+ * This file is used to export all the composables in the composables folder
+ */
+
 export { useEnergyChartData } from './solarPanel/useEnergyChartData';
 export { useShopManagement } from "./shopManagement/useShopManagement";
 export { useNavbar } from "./navbar/useNavbar";
