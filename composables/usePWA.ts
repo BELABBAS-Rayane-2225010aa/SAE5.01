54eac764c8c44c4e844f5e357d0850be5a756d26
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2024-09-16 16:06:09 +0200

    ADD commentaires usePWA.ts

--- composables/usePWA.ts
+++ composables/usePWA.ts
@@ -1,13 +1,24 @@
+/**
+ * Provides a function to check if the app is running as a Progressive Web App (PWA).
+ *
+ * @returns {Object} An object containing the checkIfPWAIsUsed function.
+ */
 export const usePWA = () => {
+  /**
+   * Checks if the app is running in standalone mode (PWA).
+   *
+   * @returns {boolean} True if the app is in standalone mode, otherwise false.
+   */
   const checkIfPWAIsUsed = () => {
     let displayMode = "browser tab";
     if (window.matchMedia("(display-mode: standalone)").matches) {
       displayMode = "standalone";
     }
 
-    // Check if the app is in standalone mode (PWA)
+    // Return true if the app is in standalone mode (PWA)
     return displayMode === "standalone";
   };
 
+  // Return the checkIfPWAIsUsed function
   return { checkIfPWAIsUsed };
 };
