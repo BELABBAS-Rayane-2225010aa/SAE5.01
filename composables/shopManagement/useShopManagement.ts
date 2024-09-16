ffbb3f550ebb300b1dd62b137f209a7b764b3a38
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2024-09-16 16:12:54 +0200

    ADD commentaires useShopManagement.ts

--- composables/shopManagement/useShopManagement.ts
+++ composables/shopManagement/useShopManagement.ts
@@ -6,21 +6,38 @@ type UseShopManagementReturn = {
   loadState: (shops: Shop[]) => Shop[];
 };
 
+/**
+ * Provides functions to manage shop data, including loading state and days.
+ *
+ * @returns {UseShopManagementReturn} An object containing the loadState function.
+ */
 export const useShopManagement = (): UseShopManagementReturn => {
   const { currentWeekNumber } = useWeek();
 
+  /**
+   * Loads the default days for a week.
+   *
+   * @returns {Day[]} An array of default days.
+   */
   const loadDefaultDays = (): Day[] => {
     return Array.from(
-      { length: 7 },
-      (_, index): Day => ({
-        day: moment()
-          .isoWeekday(index + 1)
-          .format("dddd"),
-        ...DEFAULT_DAYS,
-      }),
+        { length: 7 },
+        (_, index): Day => ({
+          day: moment()
+              .isoWeekday(index + 1)
+              .format("dddd"), // Format the day name
+          ...DEFAULT_DAYS,
+        }),
     );
   };
 
+  /**
+   * Loads the days for a given shop and week.
+   *
+   * @param {Shop} shop - The shop object.
+   * @param {number} currentWeek - The current week number.
+   * @returns {Day[]} An array of days for the given shop and week.
+   */
   const loadDays = (shop: Shop, currentWeek: number): Day[] => {
     if (shop.currentWeek.number === currentWeek) {
       return shop.currentWeek.days;
@@ -31,6 +48,12 @@ export const useShopManagement = (): UseShopManagementReturn => {
     }
   };
 
+  /**
+   * Loads the state for an array of shops.
+   *
+   * @param {Shop[]} shops - An array of shop objects.
+   * @returns {Shop[]} An array of shops with updated state.
+   */
   const loadState = (shops: Shop[]): Shop[] => {
     return shops.map((shop: Shop) => ({
       ...shop,
@@ -45,6 +68,7 @@ export const useShopManagement = (): UseShopManagementReturn => {
     }));
   };
 
+  // Return the loadState function
   return {
     loadState,
   };
