f3e45979f5af90dad01277fb6b9899f36ef2d053
victorSejourne <victor.sejourne@etu.univ-amu.fr>
2024-09-17 23:47:24 +0200

    add comments to useBackground.ts

--- composables/navbar/useBackground.ts
+++ composables/navbar/useBackground.ts
@@ -2,16 +2,19 @@ type UseBackgroundProps = {
   navbar: Ref<Element | null>;
 };
 
+//pecifies that the composable will return two functions: changeBackground and onScroll
 type UseBackgroundReturn = {
   changeBackground: () => void;
   onScroll: () => void;
 };
 
+// Declares the useBackground composable function
 export const useBackground = ({
   navbar,
 }: UseBackgroundProps): UseBackgroundReturn => {
   const route = useRoute();
 
+  // This function changes the background color of the navbar based on the scroll position or route path.
   const changeBackground = (): void => {
     if (navbar.value) {
       if (document.documentElement.scrollTop > 0 || route.path !== "/") {
@@ -30,6 +33,7 @@ export const useBackground = ({
     }
   };
 
+  // Add a backgroud to the navbar when the user as scrolled down and is not on the main page
   const onScroll = (): void => {
     if (navbar.value && route.path === "/") {
       if (window.scrollY > 0) {
