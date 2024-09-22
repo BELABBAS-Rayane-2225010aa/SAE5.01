47ba8d197917acd97325ebafaf46d3da76c12ac8
Crespin Alexandre <126071849+CRESPIN-Alexandre-2225022aa@users.noreply.github.com>
2024-09-22 15:16:31 +0200

    feat : ajout des différente données sur la page d'un event
    obliguation de refaire un appel à l'API compte tenu du nombre et de la longueur des informations.
    Impossibilité de passé par le route.
    Ref : #14

--- pages/evenement/[id].vue
+++ pages/evenement/[id].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { Event } from "~/models/event";
 
 // page available without authentification
 definePageMeta({
@@ -8,14 +9,42 @@ definePageMeta({
 // get the informations from the route
 const route = useRoute();
 const id = route.params.id;
+
+// Fetch the event from the `/api/events/:id` endpoint using a GET request.
+// It expects a response of an `Event` object and stores it in the `event` variable.
+const { data: event, error } = await useFetch<Event>(`/api/event/${id}`, {
+  method: "GET",
+});
+
+// Handle potential errors
+if (error.value) {
+  console.error("Failed to fetch event:", error.value);
+}
 </script>
 
 <template>
-    <GlobalWrapper class="shop-wrapper">
-        <MainTitle :text="`Evenement ${id}`" />
+    <GlobalWrapper class="event-wrapper">
+        <MainTitle :text="`${event?.title}`" />
+        <NuxtImg :src="event?.images[0] ?? ''" />
+        <p>{{ event?.description }}</p>
+        <p>{{ event?.date }} {{ event?.time }}</p>
+        <p>{{ event?.location }}</p>
+        <p>
+          <ul>
+            <li v-for="link in event?.links">
+              {{ link }}
+            </li>
+          </ul>
+        </p>
+        <p>
+          <ul>
+            <li v-for="(img, index) in event?.images.slice(1)" :key="index">
+              <NuxtImg :src="img ?? ''" />
+            </li>
+          </ul>
+        </p>
     </GlobalWrapper>
 </template>
   
 <style scoped>
-/* Ajoutez vos styles ici */
 </style>
