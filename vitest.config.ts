5cfaff9a59f02483fe1dc0629af573d68744293e
victorSejourne <victor.sejourne@etu.univ-amu.fr>
2024-09-24 16:48:07 +0200

    done setting up vitest and tested useDate.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,16 +3,21 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
-    // you can optionally set Nuxt-specific environment options
-    // environmentOptions: {
-    //   nuxt: {
-    //     rootDir: fileURLToPath(new URL('./playground', import.meta.url)),
-    //     domEnvironment: 'happy-dom', // 'happy-dom' (default) or 'jsdom'
-    //     overrides: {
-    //       // other Nuxt config you want to pass
-    //     }
-    //   }
-    // }
+    environmentOptions: {
+      nuxt: {
+        mock: {
+          intersectionObserver: true,
+          indexedDb: true,
+        }
+        // you can optionally set Nuxt-specific environment options
+        //
+        // rootDir: fileURLToPath(new URL('./playground', import.meta.url)),
+        // domEnvironment: 'happy-dom', // 'happy-dom' (default) or 'jsdom'
+        // overrides: {
+          // other Nuxt config you want to pass
+        // }
+      }
+    }
   }
   // any custom Vitest config you require
 })
