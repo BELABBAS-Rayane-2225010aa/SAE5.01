e0a97d7a41df6bfa18755563424632b190309156
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2024-09-24 18:28:07 +0200

    feat (API Weatherbit) : lecture des données météo dans meteo.vue ref Implémenter L'api Weatherbit #15

--- pages/meteo.vue
+++ pages/meteo.vue
@@ -1,6 +1,10 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
 
+definePageMeta({
+  auth: false,
+});
+
 export default defineComponent({
   name: "WeatherComponent",
   setup() {
@@ -25,14 +29,23 @@ export default defineComponent({
 
 
 <template>
-  <div>
-    <h1>Météo pour {{ weather?.cityName }}</h1>
+  <GlobalWrapper>
+    <div class="container photovoltaique-wrap mx-auto text-lg">
+      <PageHeader image="/photovoltaique/header.png" image-alt="Panneaux photovoltaïques"
+                  title="Panneaux photovoltaïques" />
+
+      <section class="mt-5 description">
+        <h2 class="font-extrabold text-3xl text-_primary-500">Météo pour {{ weather?.cityName }}</h2> 
     <p>Température : {{ weather?.temperatureValue }}°C</p>
     <p>Description : {{ weather?.temperatureDescription }}</p>
+    <p>oula</p>
     <p>Lever du soleil : {{ weather?.sunRise }}</p>
     <p>Coucher du soleil : {{ weather?.sunSet }}</p>
     <img :src="`https://www.weatherbit.io/static/img/icons/${weather?.weatherIcon[0]}.png`" alt="Icon météo" />
-  </div>
+        </section>
+    </div>
+  </GlobalWrapper>
+
 </template>
 
 
