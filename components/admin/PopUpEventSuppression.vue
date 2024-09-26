e3ca2843d40705e57dcd211e2f4e035e10339e9b
Crespin Alexandre <126071849+CRESPIN-Alexandre-2225022aa@users.noreply.github.com>
2024-09-26 16:24:20 +0200

    feat : CSS de la popup de suppression d'event
    Ref : #19

--- components/admin/PopUpEventSuppression.vue
+++ components/admin/PopUpEventSuppression.vue
@@ -6,35 +6,14 @@ const emit = defineEmits(['confirm', 'cancel']);  // Emit the event to confirm o
   <!-- Display the confirmation popup -->
   <div class="popup">
     <div class="popup-content">
-      <h2>Confirmer la suppression</h2>
+      <p class="popupTitle">Confirmer la suppression</p>
       <p>Êtes-vous sûr de vouloir supprimer cet événement ?</p>
-      <button @click="$emit('confirm')">Oui</button>
-      <button @click="$emit('cancel')">Non</button>
+      <UButton @click="$emit('confirm')"  class="submitBtn">Oui</UButton>
+      <UButton @click="$emit('cancel')"  class="submitBtn">Non</UButton>
     </div>
   </div>
 </template>
 
 <style scoped>
-.popup {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.popup-content {
-  background: rgb(0, 0, 0);
-  padding: 20px;
-  border-radius: 5px;
-  width: 300px;
-}
-
-button {
-  margin-top: 10px;
-}
+@import url("~/assets/css/admin/popUpEvent.css");
 </style>
