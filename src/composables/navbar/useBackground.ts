6380b934a010fdd7225fb119841892988e1adc59
victorSejournes22005043 <victor.sejourne@etu.univ-amu.fr>
2024-11-25 14:08:02 +0100

    feat : navbar scroll color

--- src/composables/navbar/useBackground.ts
+++ src/composables/navbar/useBackground.ts
@@ -22,7 +22,7 @@ export class BackgroundState {
       const maxScroll = 1200;
       const opacity = Math.min(scrollTop / maxScroll, 1);
       if (this.navbar) {
-        this.navbar.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+        this.navbar.style.backgroundColor = `rgba(20, 20, 20, ${opacity})`;
         this.navbar.style.transition = 'background-color 0.3s ease';
       }
     }
