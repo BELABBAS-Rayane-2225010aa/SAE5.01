d6dec3d1428baca3a7fbf3e1003d05433f3a3fce
BELABBAS-Rayane-2225010aa <rayanebelabbas15@gmail.com>
2025-01-14 16:23:27 +0100

    feat : commentaire dans components/card

--- src/components/card/technologie/cardDescription.ts
+++ src/components/card/technologie/cardDescription.ts
@@ -2,26 +2,28 @@ import { html, LitElement } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 import { style } from '../../../styles/technologie/cardDescription';
 import '../cardPresentation';
-
 import { Techno } from '../../../models/techno';
 
+// Define a new custom element with the name 'app-techno-description'
 @customElement('app-techno-description')
 export class CardDescription extends LitElement {
-    @property({ type: Object }) technoCard: Techno = {
-        title: '',
-        icon: '',
-        image: '',
-        shortDescription: '',
-        functioningImage: '',
-        installImage: '',
-        functioningDescription: '',
-        installDescription: '',
-        link: '',};
+  // Define a property 'technoCard' of type Techno with default values
+  @property({ type: Object }) technoCard: Techno = {
+    title: '',
+    icon: '',
+    image: '',
+    shortDescription: '',
+    functioningImage: '',
+    installImage: '',
+    functioningDescription: '',
+    installDescription: '',
+    link: '',
+  };
 
-    static styles = [
-        style
-        ];
+  // Apply the imported styles to this component
+  static styles = [style];
 
+  // Render method to describe the component's template
   render() {
     return html`
       <div class="description__container" id="${this.technoCard.link}">
